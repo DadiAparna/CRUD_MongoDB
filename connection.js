fb0a3ec7d658c4762d33a2f69e43b05a3bede188
DadiAparna <daparna002@gmail.com>
2023-03-27 07:59:10 +0530

    Initial Commit

--- connection.js
+++ connection.js
@@ -7,7 +7,7 @@ const connectionParams = {
 }
 
 const url = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.pmc0bct.mongodb.net/?retryWrites=true&w=majority`
-console.log('url',url)
+//console.log('url',url)
 const connexion = mongoose.connect(url, connectionParams).then(() => console.log('connected to cloud atlas'))
 
 .catch((err) => console.log('err',err));
